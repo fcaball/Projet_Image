0554f3edfd70812986ae8e91bffa88e9b4d89ade
fabienCaballero <fabest.09@orange.fr>
2023-04-25 11:05:36 +0200

    PRESENTATION FINALE

---
+++ Projet_Image/mosaiquemaker.cpp
@@ -0,0 +1,6 @@
+#include "mosaiquemaker.h"
+
+mosaiquemaker::mosaiquemaker()
+{
+
+}
---
+++ Projet_Image/mosaiquemaker.h
@@ -0,0 +1,11 @@
+#ifndef MOSAIQUEMAKER_H
+#define MOSAIQUEMAKER_H
+
+
+class mosaiquemaker
+{
+public:
+    mosaiquemaker();
+};
+
+#endif // MOSAIQUEMAKER_H
